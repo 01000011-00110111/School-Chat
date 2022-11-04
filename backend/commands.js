940a98ddf98398841aad2f6be7a1ce9ceff02214
cserver45 <77689658+cserver45@users.noreply.github.com>
2022-11-04 23:26:01 +0000

    Add prep work for new command system

---
+++ backend/commands.js
@@ -0,0 +1,24 @@
+function is_cmd(cmd) {
+  // check if the text could match a cmd
+  // MAKE SURE THIS ONLY MATCHES CMDS AND NOT A CHAT MESSAGE WITH THE WORD HELP AS AN EXAMPLE
+}
+
+function help_cmd() {
+  // open static/commands.txt, read all commands
+  // open static/cmd-def.txt, read all def for commands
+  // send user_name as SYSTEM for these commands
+}
+
+// Mute cmd list and checks
+
+function mute_cmd(user) {
+  // take the user provided and add them to the muted.txt file
+}
+
+function unmute_cmd(user) {
+  // remove the user provided from te muted.txt file
+}
+
+function is_user_muted(user) {
+  // check if user is in muted.txt
+}
