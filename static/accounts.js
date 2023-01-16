121f4655c116056889f6acd5a74e97aa3fb04bc5
C7 <113735381+01000011-00110111@users.noreply.github.com>
2023-01-16 01:48:17 +0000

    restarting logins

--- static/accounts.js
+++
@@ -1,7 +0,0 @@
-function openForm() {
-  document.getElementById("accountlogin").style.display = "block";
-}
-
-function closeForm() {
-  document.getElementById("accountlogin").style.display = "none";
-}
