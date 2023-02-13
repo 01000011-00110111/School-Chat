6e03ff07f5bc16b90e1bbef575fb29cf53986c6c
C7 <113735381+01000011-00110111@users.noreply.github.com>
2023-02-13 00:28:31 +0000

    Start to set roles

--- static/accounts.js
+++ static/accounts.js
@@ -2,3 +2,13 @@ function pass() {
     ismutted = 'false'
     document.cookie = "permission=" + ismutted + "; path=/";
 }
+
+
+
+
+
+// set roles 
+
+function Testrole() {
+    let role = "Test"
+}
