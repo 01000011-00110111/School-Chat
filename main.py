ef6330d5ee0331dec058a51515aa822ade8ced72
cserver45 <cservergaming@gmail.com>
2023-09-28 17:32:37 +0000

    docs: add docstrings to User class (and make pylint happy)

--- main.py
+++ main.py
@@ -75,40 +75,56 @@ dbm.Online.delete_many({})
 
 
 class User:
+    """Represents a logged in user."""
 
     def __init__(self, username):
+        """Initialize the user."""
         self.username = username
 
     @staticmethod
     def is_authenticated():
+        """Check if the user is authenitcated."""
+        # this would only be used if we needed to check 2fa or something like that.
         return True
 
     @staticmethod
     def is_active():
+        """Check if the user's session is recent."""
+        # we could implement some kind of token expire here I think
         return True
 
     @staticmethod
     def is_anonymous():
+        """Check if the user is anonymous (never will be)."""
+        # We disabled anonymous users a while ago
         return False
 
     def get_id(self):
+        """Return the user's username."""
+        # whenever we get arround to it, maybe switch this to userid?
         return self.username
 
     @staticmethod
     def check_password(password_hash, password):
+        """Check the user's password against the one entered in the login field."""
         return hashlib.sha384(bytes(password,
                                     'utf-8')).hexdigest() == password_hash
 
     @staticmethod
     def check_username(username, db_username):
+        """Check the username against the one entered in the login field."""
         return username == db_username
 
+    # pylint: disable=E0213
     @login_manager.user_loader
     def load_user(username):
+        """Load the user into flask-login."""
         u = dbm.Accounts.find_one({"username": username})
         if not u:
             return None
         return User(username=u['username'])
+
+    # pylint: enable=E0213
 
 
 # license stuff
@@ -563,7 +579,6 @@ def get_rooms(username):
              to=request.sid)
 
 
-# pylint: disable=C0103
 @socketio.on('message_chat')
 def handle_message(user_name, message, roomid, userid):
     """New New chat message handling pipeline."""
@@ -586,9 +601,6 @@ def handle_message(user_name, message, roomid, userid):
             filtering.failed_message("return", roomid, user)
     else:
         filtering.failed_message(result, roomid, user)
-
-
-# pylint: enable=C0103
 
 
 @socketio.on('pingtest')
