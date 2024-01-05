806671ee65154571cc59f5dba844558a2ec22ac0
cserver45 <cservergaming@gmail.com>
2024-01-04 20:45:01 -0500

    and remove the old system from client-side

--- static/chat.js
+++ static/chat.js
@@ -108,17 +108,6 @@ function changeRoom(room) {
     socket.emit('room_connect', room)
 }
 
-function toHyperlink(str) {
-    var pattern1 = /(\b(https?|ftp|sftp|file|http):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
-    var str1 = str.replace(pattern1, "<a href='$1'>$1</a>");
-    var pattern2 = /(^|[^\/])(www\.[\S]+(\b|$))/gim;
-    var str2 = str1.replace(pattern2, '$1<a target="_blank" href="http://$2">$2</a>');
-    var pattern3 = /mailto:([^\?]*)/gm;
-    var str3 = str2.replace(pattern3, "<a href='mailto:$1'>$1</a>");
-
-    return str3;
-}
-
 // function BTMLog() {
 //   if (Math.floor(window.scrollY) === window.scrollMaxY) {
 //     console.log("cheese");
@@ -140,10 +129,9 @@ function sendMessage() {
     }
 
     let chatDiv = document.getElementById("chat");
-    messageL = toHyperlink(message);
     messageElement["value"] = "";
     // this is needed, because this goes over socketio, not a normal http request
-    socket.emit('message_chat', user, messageL, window.sessionStorage.getItem("roomid"), userid);
+    socket.emit('message_chat', user, message, window.sessionStorage.getItem("roomid"), userid);
     window.scrollTo(0, chatDiv.scrollHeight);
 }
 
