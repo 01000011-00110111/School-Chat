284e9a32dfb39bcf00ee3ae27ac0605ba813a301
01000011-00110111 <113735381+01000011-00110111@users.noreply.github.com>
2024-01-29 16:13:53 +0000

    actually fixed it this time

--- database.py
+++ database.py
@@ -572,7 +572,7 @@ def find_private_messages(userlist, sender):
 def send_private_message(message, pmid, userid):
     """sends the message to the private chat room"""
     unread = Private.find_one({"pmid": pmid})
-    reciver = unread['userIds'].pop(userid).values()
+    reciver = unread['userIds'].remove(userid)[0]
     unread['unread'][reciver] += 1
     #later i might make better but there fixed
     Private.update_one({"pmid": pmid},
