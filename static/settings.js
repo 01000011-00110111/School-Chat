3146ba474e2b6d9284549c7c652fb23507d04dfe
CastyiGlitchxz <90124265+CastyiGlitchxz@users.noreply.github.com>
2024-02-19 15:52:00 -0500

    Most likey finished with this

--- static/settings.js
+++ static/settings.js
@@ -164,4 +164,28 @@ function openTab(evt, tabName) {
   }
   
   // Opens the defualt tab
-  openTab(event, "My-Profile")
+  openTab(event, "My-Profile");
+
+  function enableNotifications() {
+    // Checks if the user's browser supports notifications
+    if (!"Notification" in window) {
+      
+      alert("Your browser does not support desktop notifications.");
+  
+      // It checks if the user already has notifications enabled for this site
+    } else if (Notification.permission === "granted") {
+      
+      const notification = new Notification("Notifications Enabled");
+  
+      // If user has notifications disbaled then it asks for permission to enable them
+    } else if (Notification.permission !== "denied") {
+      
+      Notification.requestPermission().then((permission) => {  
+        if (permission === "granted") {
+          
+          const notification = new Notification("Notifications Enabled");
+          
+        }
+      });
+    }
+  }
